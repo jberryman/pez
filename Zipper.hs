50ed7dc1eff3c883eeba26fa10cd53f375119866
brandon.m.simmons <brandon.m.simmons@gmail.com>
2010-10-07 16:59:12 -0400

    It compiles!
    Ignore-this: 9016bf49926d1f75c6fed7f40eb8ac88
    Figured out the issue was I needed a wrapper type to make a proper thrist from both (;->) and (->)
    
    darcs-hash:20101007205912-a5925-da302b66274df6cf5358fb1ffbba548004cfcd09.gz

--- Zipper.hs
+++ Zipper.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, TemplateHaskell, DeriveDataTypeable #-}
+{-# LANGUAGE TypeOperators, TemplateHaskell, GADTs, DeriveDataTypeable #-}
 module Zipper (
     -- * Export Typeable and fclabels, for convenience:
       module Data.Record.Label
@@ -9,10 +9,10 @@ module Zipper (
     , moveUp
     , focus
 
-{-    
     , zipper 
     , close
     , closeSaving
+{-    
 
     , Saved -- stores our lens
     , save  --extracts a Saved from zipper
@@ -43,63 +43,95 @@ import Control.Category
 import Prelude hiding ((.), id) -- take these from Control.Category
 
 
-{-
- - the 'a' parameter is both the root and the "step" type. Meaning
- - moving up will always yield a 'Zipper a a', however moving down
- - polymorphically is allowed. Thus 'b' is the type of the current 
- - focus.
-data Zipper a b = 
-         -- a stack that lets us ascend a step at a time up the data structure
-    Z { lensesConts :: [ (a :-> a, a -> a) ],                  
-         -- as we descend we compose types here until they can be placed on
-         -- the lensesConts stack:                                                  
-        typeBuffer  :: Maybe ( a :-> b, a -> b ),                
-        _focus      :: b                                  
-       } deriving (Typeable)
- -}
-
 {- 
+ - DESCRIPTION:
+ -
  - we use a Thrist to create a type-threaded stack of continuations
  - allowing us to have a polymorphic history of where we've been.
  - by using Typeable, we are able to "move up" by type casting in
  - the Maybe monad. This means that the programmer has to know
  - what type a move up will produce, or deal with unknowns.
+ -
+ -TODO: consider instead of using section, use head form of parent with
+ -      the child node set to undefined. Any performance difference?
  -}
-data Zipper a b = 
-         -- stacks that let us ascend a step at a time up the data structure
-    Z { lenses ::  Thrist (:->) a b,                  
-        conts  :: Thrist (->) b a,
-        _focus  :: b                                  
-      } deriving (Typeable)
 
 
+
+    -------------------------
+    -- TYPES: the real heros
+    ------------------------
+
+
+ -- We store our history in a type-threaded list of pairs of lenses and
+ -- continuations (parent data-types with a "hole" where the child fits):
+ --    Use GADT to enforce Typeable constraint
+data HistPair b a where 
+    H :: (Typeable a, Typeable b)=> (a :-> b) -> (b -> a) -> HistPair b a
+
+type ZipperStack b a = Thrist HistPair b a
+
+data Zipper a b = Z { stack  :: ZipperStack b a,
+                      _focus :: b                                  
+                    } deriving (Typeable)
+    
  -- generate the lens we export for 'focus' using fclabel's TH:
 $(mkLabels [''Zipper])
 
-moveTo :: (Typeable c)=> (b :-> c) -> Zipper a b -> Zipper a c
-moveTo = undefined
-{-
-moveTo l (Z cs mb f) = do let c  = (l , f `missing` l) 
-                              f' = getL l f
-                          a <- gcast ...
--}
 
-moveUp :: Zipper a b -> Zipper a a
-moveUp z = undefined
+ -- | stores the path used to return to the same location in a data structure
+ -- as the one we just exited. You can also extract a lens from a Saved that
+ -- points to that location:
+data Saved a b = S (Thrist (:->) a b)
+
+
+
+    ---------------------------
+    -- Basic Zipper Functions:
+    ---------------------------
+
+
+-- TODO: - make below use some error handling for Maybe:
+ --
+ -- | Move down the structure to the label specified. Return Nothing if the
+ -- label is not valid for the focus's constructor:
+moveTo :: (Typeable b, Typeable c)=> (b :-> c) -> Zipper a b -> Maybe (Zipper a c)
+moveTo l (Z stck b) = let f = b `missing` l 
+                          c = getL l b      
+                       in Just $ Z (Cons (H l f) stck) c
+
+ -- | Move up a level as long as the type of the parent is what the programmer
+ -- is expecting and we aren't already at the top. Otherwise return Nothing.
+moveUp :: (Typeable c, Typeable b)=> Zipper a c -> Maybe (Zipper a b)
+moveUp (Z Nil _)                 = Nothing --already at top
+moveUp (Z (Cons (H _ f) stck) c) = gcast $ Z stck $ f c
 
 
 zipper :: (Typeable a)=> a -> Zipper a a
-zipper a = undefined
+zipper = Z Nil
+
+
+close :: Zipper a b -> a
+close = snd . closeSaving
+
+
+closeSaving :: Zipper a b -> (Saved a b, a)
+--closeSaving (Z lThr cThr b) = (S lThr, compStack cThr b)
+closeSaving = undefined
+
+
 
 
     ------------
     -- HELPERS
     ------------
 
+
  -- make a hole in a type corresponding to the passed lens, forming a section:
  --  MAYBE GIVE THE GC SOME HINTS HERE?:
 missing :: a -> (a :-> b) -> (b -> a)
 missing a l = flip (setL l) a
+
 
  -- fold a thrist into a single category by composing the stack with (.)
  -- Here 'cat' will be either (->) or (:->):
